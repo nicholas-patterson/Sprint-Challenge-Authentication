8c8160f17d135b3a6e595ad727ba4a28c7a00e29
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-20 11:40:13 -0400

    :sparkles: adds auth router and helpers
    adds auth router and helpers

---
+++ auth/auth-helpers.js
@@ -0,0 +1,25 @@
+const db = require("../database/dbConfig");
+
+module.exports = {
+  findById,
+  register,
+  login
+};
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+function register(newUser) {
+  return db("users")
+    .insert(newUser)
+    .then(([id]) => {
+      return findById(id);
+    });
+}
+
+function login(filter) {
+  return db("users").where(filter);
+}
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,34 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Users = require("./auth-helpers");
+const bcrypt = require("bcrypt");
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  const hash = bcrypt.hashSync(password, 10);
+
+  Users.register({ username, password: hash })
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({ error: `Server could not register user` });
+    });
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  Users.login({ username })
+    .then(user => {
+      const authenticate = bcrypt.compareSync(password, user.password);
+      if (user && authenticate) {
+        res.status(200).json({ message: `Welcome, ${user.username}` });
+      } else {
+        res.status(400).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(500)
+    .json({ error: "Sever could not log user in" });
 });
 
 module.exports = router;
